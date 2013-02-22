507be1145e7b836c3ba5c499ad2b46fd337668c7
Andrey Tovchigrechko <atovtchi@jcvi.org>
2013-02-21 19:32:47 -0500

    Unpack and setup JBrowse from postinstall entry point

--- lib/VICVB/postinstall.py
+++ lib/VICVB/postinstall.py
@@ -2,21 +2,33 @@ from pkg_resources import Requirement, resource_filename
 from glob import glob
 import os, shutil, zipfile
 from argh import *
+from subprocess import check_call
+import logging
+
+log = logging.getLogger(__name__)
 
 pjoin = os.path.join
 
-def install_jbrowse(data_dir,install_dir):
-    jbrowse_zip = glob(pjoin(data_dir,"JBrowse*"))
+pkg_name = "VICVB"
+pkg_data_dir = resource_filename(Requirement.parse(pkg_name),"VICVB/data")
+
+def install_jbrowse(install_dir):
+    jbrowse_zip = glob(pjoin(pkg_data_dir,"JBrowse*"))
     assert len(jbrowse_zip) == 1, "Expected a single JBrowse archive "+\
-            "stored within the package data dir: %s" % (data_dir,)
+            "stored within the package data dir: %s" % (pkg_data_dir,)
     jbrowse_zip = jbrowse_zip[0]
     #context manager only works for zipfile in Python 2.7
-    with zipfile.ZipFile(jbrowse_zip, 'r') as f:
+    f = zipfile.ZipFile(jbrowse_zip, 'r')
+    try:
         f.extractall(path=install_dir)
+        install_name = os.path.dirname(f.namelist()[0])
+    finally:
+        f.close()
+    install_home = pjoin(install_dir,install_name)
+    check_call(["./setup.sh"],cwd=install_home)
+    return install_home
     
 def install_tools():
-    pkg_name = "VICVB"
-    data_dir = resource_filename(Requirement.parse(pkg_name),"data")
     install_jbrowse(data_dir,install_dir)
 
 def main():
